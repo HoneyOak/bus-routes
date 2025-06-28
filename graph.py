c279419ae96f698b2846827865468c8b603ad78a
HoneyOak <divyam.seksaria2702@gmail.com>
2025-06-28 20:22:35 +0530

    Found all bus stops in a radius with a given center

--- graph.py
+++ graph.py
@@ -3,12 +3,12 @@ import pandas as pd
 from jaal import Jaal
 
 G = nx.DiGraph()
-G.add_edge("A", "B", weight=10)
-G.add_edge("B", "A", weight=6)
-G.add_edge("B", "C", weight=3)
+G.add_edge("Dadar Fire brigade", "Madhav Nagar", weight=6)
+G.add_edge("Madhav Nagar", "Dadar Fire brigade", weight=7)
 
 # Convert to DataFrame
 edges = pd.DataFrame([(u, v, str(d["weight"])) for u, v, d in G.edges(data=True)],
                      columns=['from', 'to', 'label'])
 
 Jaal(edges).plot()
+
